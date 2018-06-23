a2d08fc92c373b8283b6fae216943aaae5f19d05
Behruz Isoev <isaev.behruz@gmail.com>
2018-06-23 15:39:13 +0500

    Install RxJS & minor modification

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import * as _ from "lodash";
 
+import { range } from "rxjs";
+import { filter, map } from "rxjs/operators";
+
 import "./app.scss";
 
 if (process.env.NODE_ENV === "development") {
@@ -7,3 +10,7 @@ if (process.env.NODE_ENV === "development") {
 }
 
 console.log(_.join(["Hello", "webpack"], " "));
+
+range(1, 10)
+    .pipe(filter((x: number) => x % 2 === 1), map((x: number) => x + x))
+    .subscribe((x: number) => console.log(x));
